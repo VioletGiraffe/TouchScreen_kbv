3c74b01ba96615ca8b1e2d080725c1c2192b1081
VioletGiraffe <violet.giraffe@noreply.github.com>
2019-07-28 16:13:23 +0300

    Improvements

--- TouchScreen_kbv.h
+++ TouchScreen_kbv.h
@@ -3,37 +3,49 @@
 // (c) ladyada / adafruit
 // Code under MIT License
 
-#ifndef _TOUCHSCREEN_KBV_H_
-#define _TOUCHSCREEN_KBV_H_
+#pragma once
 #include <stdint.h>
 
-class TSPoint_kbv {
-    public:
-        TSPoint_kbv(void);
-        TSPoint_kbv(int16_t x, int16_t y, int16_t z);
+class TSPoint_kbv
+{
+public:
+	TSPoint_kbv() = default;
+	inline TSPoint_kbv(int16_t x0, int16_t y0, int16_t z0) : x{x0}, y{y0}, z{z0} {
+	}
 
-        bool operator==(TSPoint_kbv);
-        bool operator!=(TSPoint_kbv);
+	inline bool operator==(const TSPoint_kbv& other) const {
+		return x == other.x && y == other.y && z == other.z;
+	}
 
-        int16_t x, y, z;
+	inline bool operator!=(const TSPoint_kbv& other) const {
+		return !(*this == other);
+	}
+
+	int16_t x = 0, y = 0, z = 0;
 };
 
-class TouchScreen_kbv {
-    public:
-        TouchScreen_kbv(uint8_t xp, uint8_t yp, uint8_t xm, uint8_t ym);
-        TouchScreen_kbv(uint8_t xp, uint8_t yp, uint8_t xm, uint8_t ym, uint16_t rx);
+class TouchScreen_kbv
+{
+public:
+	TouchScreen_kbv(uint8_t xp, uint8_t yp, uint8_t xm, uint8_t ym);
+	TouchScreen_kbv(uint8_t xp, uint8_t yp, uint8_t xm, uint8_t ym, uint16_t rx);
 
-//        bool isTouching(void);
-        uint16_t pressure(void);
-        int readTouchY();
-        int readTouchX();
-        TSPoint_kbv getPoint();
-        int16_t pressureThreshhold;
+	uint16_t pressure();
+	int readTouchY();
+	int readTouchX();
+	TSPoint_kbv getPoint();
 
-    private:
-        uint8_t _yp, _ym, _xm, _xp;
-        uint16_t _rxplate;
+	TSPoint_kbv getPointSafe() {
+		const auto tp = getPoint();
+		pinMode(_yp, OUTPUT); //restore shared pins
+		pinMode(_xm, OUTPUT);
+		digitalWrite(_yp, HIGH); //because TFT control pins
+		digitalWrite(_xm, HIGH);
+
+		return tp;
+	}
+
+private:
+	uint8_t _yp, _ym, _xm, _xp;
+	uint16_t _rxplate;
 };
-
-#endif
-
